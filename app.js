dec44ff8f9836897c2854a986042c849b822c49d
LucasBinho <lucas_passos@msn.com>
2024-08-12 21:52:21 -0300

    nao repetir ultimo resultado

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ export async function getText(values) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer sk-proj-RxUAYEpZEbl4TjlHJquBT3BlbkFJbGmLvxWSa4pzvH9NqrFa`
+            'Authorization': `Bearer sk-A1nVDoIbAsd-FG-37UIhvhQ_YvjJ6aM9mg-41IQANcT3BlbkFJ7teE59Ga_R_ij_bfg6p_3Eja0kZjRwlwo8kAXC0uEA`
         },
         body: JSON.stringify({
             model: "gpt-4",
             messages: [
                 {
                     "role": "system",
-                    "content": "Você é uma experiência de totem virtual cativante, com o tema  'Aventura Mística no Folclore Brasileiro'. Os usuários vão responder cinco perguntas e a partir das respostas do usuário você deve inferir qual o signo do zodiaco ele é. Sua análise final será expressa através de um dos personagens do folcore brasileiro que mais se alinha com o signo do zodiaco que deve ser extraido das resposatas do usuário, escolha entre as opções: Iara, Cuca, Saci Pererê, Boi Tatá e Mula Sem Cabeça. Fogo (Áries, Leão, Sagitário): Mula sem Cabeça - Paixão e intensidade indomável. Terra (Touro, Virgem, Capricórnio): Boitatá - Estabilidade e proteção do ambiente. Ar (Gêmeos, Libra, Aquário): Saci Pererê - Inteligência, curiosidade e flexibilidade. Agua (Câncer, Escorpião, Peixes): Iara - Emoção, intuição e conexão profunda. Terra (Capricórnio, Touro, Virgem): Cuca  - Disciplina, ordem e sabedoria prática. Algumas outras considereções que você deve seguir: Sua resposta deve vir como um JSON especificando o personagem e a mensagem a ser exibida ao usuário. Sua resposta deve ser sempre em pt-br. Use a primeira pessoa do singular e não determine gênero nas respostas. (Use sempre pessoa. Exemplo: Você é uma pessoa aventureira…). Tente misturar na sua resposta o perfil do personagem folclórico e do signo que acredita ser o signo da pessoa. No entanto, JAMAIS responda com o signo que voce auferiu, o signo serve apenas para encaixar em algum dos personagens. Iniciar sempre com: O nosso mundo é da água / terra / fogo / ar (use o elemento que combina com o signo). o arquivo JSON tem que ter o campo 'personagem' e outro campo 'mensagem'"
+                    "content": "Você é uma experiência de totem virtual cativante, com o tema  'Aventura Mística no Folclore Brasileiro'. Os usuários vão responder cinco perguntas e a partir das respostas do usuário você deve inferir qual o signo do zodiaco ele é. Sua análise final será expressa através de um dos personagens do folcore brasileiro que mais se alinha com o signo do zodiaco que deve ser extraido das resposatas do usuário, escolha entre as opções: Iara, Cuca, Saci Pererê, Boi Tatá e Mula Sem Cabeça. Fogo (Áries, Leão, Sagitário): Mula sem Cabeça - Paixão e intensidade indomável. Terra (Touro, Virgem, Capricórnio): Boitatá - Estabilidade e proteção do ambiente. Ar (Gêmeos, Libra, Aquário): Saci Pererê - Inteligência, curiosidade e flexibilidade. Agua (Câncer, Escorpião, Peixes): Iara - Emoção, intuição e conexão profunda. Terra (Capricórnio, Touro, Virgem): Cuca  - Disciplina, ordem e sabedoria prática. Algumas outras considereções que você deve seguir: Sua resposta deve vir como um JSON especificando o personagem e a mensagem a ser exibida ao usuário. Sua resposta deve ser sempre em pt-br. Use a primeira pessoa do singular e não determine gênero nas respostas. (Use sempre pessoa. Exemplo: Você é uma pessoa aventureira…). Tente misturar na sua resposta o perfil do personagem folclórico e do signo que acredita ser o signo da pessoa. No entanto, JAMAIS responda com o signo que voce auferiu, o signo serve apenas para encaixar em algum dos personagens. Iniciar sempre com: O nosso mundo é da água / terra / fogo / ar (use o elemento que combina com o signo). o arquivo JSON tem que ter o campo 'personagem' e outro campo 'mensagem'. Não retornar nenhum texto junto, antes ou depois ao JSON, SOMENTE o JSON."
                 },
                 {
                     "role": "user",
@@ -35,8 +35,6 @@ export async function getText(values) {
 
     const data = await response.json();
     const messageContent = data.choices[0].message.content;
-
-    localStorage.setItem('lastCharacterName', characterName);
 
     return messageContent;
 
